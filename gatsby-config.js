ea01b6a45a5c43a91ccadb3ae7d95a6a9069ba58
ry.zou <ry.zou@aftership.com>
2020-07-28 17:55:58 +0800

    :wheelchair: (pwa): enable offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     "gatsby-plugin-dark-mode",
     `gatsby-plugin-postcss`,
   ],
